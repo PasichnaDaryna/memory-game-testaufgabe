46678e9ce9484064fe2b331f8b173c2eb85ce9c8
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-06-29 12:37:06 +0200

    Card in progress
    next fix the styles of flipped card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import s from './Card'
-
+import classnames from "classnames";
 import backpic from "../../images/backpic.jpg"
 import frontpic from "../../images/image1.jpg"
-export default function Card() {
+
+
+const Card = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
+    const handleClick = () => {
+        !isFlipped && !isDisabled && onClick(index);
+    };
     return (
-        <div className="card">
+        <div
+            className={classnames("card", {
+                "flipped": isFlipped,
+                "inactive": isInactive,
+            })}
+            onClick={handleClick}
+        >
             <div className="cardFace cardFontFace">
                 <img src={backpic} className="image" alt="back-pic" />
             </div>
 
             <div className="card">
-                <div className="cardFace">
-                    <img src={frontpic} className="image" alt="front-pic" />
+                <div className="cardFace cardBackFace">
+                    <img src={card.image} className="image" alt="front-pic" />
                 </div>
 
             </div>
@@ -20,3 +30,6 @@ export default function Card() {
         </div>
     )
 }
+
+
+export default Card;
